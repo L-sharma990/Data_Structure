e6e9b5459cfe63c8c2efd8764714195816f4d5f5
L-sharma990 <lokeshsharma5057833@gmail.com>
2025-11-20 14:59:36 +0530

    Update AD_Q2.cpp

--- Assignment7/AD_Q2.cpp
+++ Assignment7/AD_Q2.cpp
@@ -1,62 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool issubstring(string str1,string str2){
-    if(str1.length()>str2.length()){
-        return false;
-    }
-    for(int i=0;i<=(str2.length() - str1.length());i++){
-        for(int j=0;j<str1.length();j++){
-            if(str2[i+j]==str1[j]){
-                return true;
-                break;
-            }
-            else{
-                return false;
-            }
-        }
-    }
+int getMax(int array[], int n) {
+  int max = array[0];
+  for (int i = 1; i < n; i++)
+    if (array[i] > max)
+      max = array[i];
+  return max;
 }
-bool checkpossible(string str){
-    int n = str.length();
 
-    for(int i=1;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            string str_part1 = str.substr(0,i);
-            string str_part2 = str.substr(i,j-i);
-            string str_part3 = str.substr(j);
+void countingSort(int array[], int size, int place) {
+  const int max = 10;
+  int output[size];
+  int count[max];
 
-            if(issubstring(str_part1,str_part2) && issubstring(str_part1,str_part3)){
-                cout<<"After spliting: "<<str_part1<<" "<<str_part2<<" "<<str_part3<<endl;
-                return true;
-            }
-            if(issubstring(str_part2,str_part1) && issubstring(str_part2,str_part3)){
-                cout<<"After spliting: "<<str_part1<<" "<<str_part2<<" "<<str_part3<<endl;
-                return true;
-            }
-            if(issubstring(str_part3,str_part1) && issubstring(str_part3,str_part2)){
-                cout<<"After spliting: "<<str_part1<<" "<<str_part2<<" "<<str_part3<<endl;
-                return true;
-            }
-        }
-    }
-    return false;
+  for (int i = 0; i < max; ++i)
+    count[i] = 0;
 
-    
+  for (int i = 0; i < size; i++)
+    count[(array[i] / place) % 10]++;
+
+  for (int i = 1; i < max; i++)
+    count[i] += count[i - 1];
+
+  for (int i = size - 1; i >= 0; i--) {
+    output[count[(array[i] / place) % 10] - 1] = array[i];
+    count[(array[i] / place) % 10]--;
+  }
+
+  for (int i = 0; i < size; i++)
+    array[i] = output[i];
 }
-int main(){
-    int n;
-    cout<<"Enter the number of strings:";
-    cin>>n;
-    string str;
-    for(int i=0;i<n;i++){
-        cout<<"Enter the "<<i+1<<" string:";
-        cin>>str;
-        if(checkpossible(str)){
-            cout<<"yes"<<endl;
-        }
-        else{
-            cout<<"No"<<endl;
-        }
-    }
+
+void radixsort(int array[], int size) {
+  int max = getMax(array, size);
+  for (int place = 1; max / place > 0; place *= 10)
+    countingSort(array, size, place);
+}
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << array[i] << " ";
+  cout << endl;
+}
+
+int main() {
+  int array[] = {121, 432, 564, 23, 1, 45, 788};
+  int n = sizeof(array) / sizeof(array[0]);
+  radixsort(array, n);
+  printArray(array, n);
 }
