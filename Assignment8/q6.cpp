4cd98b2d99c4e661eb1ce7c4432a0fe02e597269
L-sharma990 <lokeshsharma5057833@gmail.com>
2025-12-03 16:24:56 +0530

    Update q6.cpp

--- Assignment8/q6.cpp
+++ Assignment8/q6.cpp
@@ -1,41 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 class MaxHeap {
-    vector<int> h;
+    int h[100];
+    int sz;
 public:
+    MaxHeap() { sz = 0; }
     void push(int x) {
-        h.push_back(x);
-        int i = h.size() - 1;
+        h[sz] = x;
+        int i = sz;
+        sz++;
         while (i > 0) {
             int p = (i - 1) / 2;
             if (h[p] < h[i]) {
-                swap(h[p], h[i]);
+                int t = h[p]; h[p] = h[i]; h[i] = t;
                 i = p;
             } else break;
         }
     }
-    bool empty() const {
-        return h.empty();
-    }
-    int top() const {
-        if (h.empty()) throw runtime_error("empty");
-        return h[0];
-    }
+    bool empty() { return sz == 0; }
+    int top() { return h[0]; }
     void pop() {
-        if (h.empty()) return;
-        h[0] = h.back();
-        h.pop_back();
-        int n = h.size();
+        if (sz == 0) return;
+        h[0] = h[sz-1];
+        sz--;
         int i = 0;
-        while (true) {
+        while (1) {
             int l = 2 * i + 1;
             int r = 2 * i + 2;
             int largest = i;
-            if (l < n && h[l] > h[largest]) largest = l;
-            if (r < n && h[r] > h[largest]) largest = r;
+            if (l < sz && h[l] > h[largest]) largest = l;
+            if (r < sz && h[r] > h[largest]) largest = r;
             if (largest != i) {
-                swap(h[i], h[largest]);
+                int t = h[i]; h[i] = h[largest]; h[largest] = t;
                 i = largest;
             } else break;
         }
@@ -50,5 +47,4 @@ int main() {
     pq.push(10);
     int x = pq.top();
     pq.pop();
-    return 0;
 }
