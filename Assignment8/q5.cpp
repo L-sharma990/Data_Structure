4967ca64bf0b3654c21ffb91ea70c906b2b3e7b3
L-sharma990 <lokeshsharma5057833@gmail.com>
2025-12-03 16:23:50 +0530

    Update q5.cpp

--- Assignment8/q5.cpp
+++ Assignment8/q5.cpp
@@ -1,35 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void heapify(vector<int>& a, int n, int i) {
+void heapify(int a[], int n, int i) {
     int largest = i;
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
     if (l < n && a[l] > a[largest]) largest = l;
     if (r < n && a[r] > a[largest]) largest = r;
     if (largest != i) {
-        swap(a[i], a[largest]);
+        int t = a[i]; a[i] = a[largest]; a[largest] = t;
         heapify(a, n, largest);
     }
 }
 
-void heapSortIncreasing(vector<int>& a) {
-    int n = a.size();
-    for (int i = n / 2 - 1; i >= 0; --i) heapify(a, n, i);
-    for (int i = n - 1; i > 0; --i) {
-        swap(a[0], a[i]);
+void heapSortIncreasing(int a[], int n) {
+    for (int i = n/2 - 1; i >= 0; i--) heapify(a, n, i);
+    for (int i = n-1; i > 0; i--) {
+        int t = a[0]; a[0] = a[i]; a[i] = t;
         heapify(a, i, 0);
     }
 }
 
-void heapSortDecreasing(vector<int>& a) {
-    heapSortIncreasing(a);
-    reverse(a.begin(), a.end());
-}
-
 int main() {
-    vector<int> a = {4, 1, 3, 9, 7};
-    heapSortIncreasing(a);
-    heapSortDecreasing(a);
-    return 0;
+    int a[5] = {4,1,3,9,7};
+    heapSortIncreasing(a, 5);
 }
